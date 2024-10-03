4840e9aa6a41a800c760bdd07e60119136b12ccf
AroonTECH <132234727+AroonTECH@users.noreply.github.com>
2024-10-03 19:44:50 +0200

    delete files

--- resources/views/layouts/script.blade.php
+++
@@ -1,3 +0,0 @@
-<script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/js/grid.js')}}"></script>
